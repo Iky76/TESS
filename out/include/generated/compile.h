7359ec96c5bad18d69b47c26c7c0d39c607ad7ea
Iky76 <yuyenem@gmail.com>
2025-12-11 05:14:16 +0000

    fix missing Techpack

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Thu Dec 11 05:10:33 UTC 2025"
-#define LINUX_COMPILE_BY "Rizj"
-#define LINUX_COMPILE_HOST "Prjct"
-#define LINUX_COMPILER "Ubuntu clang version 18.1.3 (1ubuntu1)"
